210622fd7ba46b6400618a5437e8cc10706730ac
sr-hub <spencer.r1@outlook.com>
2019-12-16 09:30:52 -0500

    added editable, prvented new/edit on resource

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const events = require('./events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +8,5 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  events.addHandlers()
 })
