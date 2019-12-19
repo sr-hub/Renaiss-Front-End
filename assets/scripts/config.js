82034ab9f72420e5d13510b3487ebb1ed68a9f62
sr-hub <spencer.r1@outlook.com>
2019-12-19 16:14:37 -0500

    removed slash from api

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://damp-brushlands-43768.herokuapp.com/',
+  production: 'https://damp-brushlands-43768.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
